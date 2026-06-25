8737db56a581291ca27eade4924c4eb8e9920204
RomahaPoleha <romashkoo2023@list.ru>
2026-06-25 13:50:20 +0300

    Исправлен apps

--- screener/apps.py
+++ screener/apps.py
@@ -1,10 +1,19 @@
 from django.apps import AppConfig
-from .natr_updater import start_natr_updater
+
 
 class ScreenerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'screener'
 
     def ready(self):
-        # Запускаем только NATR updater
-        start_natr_updater()
+        try:
+            from .candle_updater import start_candle_updater
+            start_candle_updater()
+        except Exception as e:
+            print(f"⚠️ Candle updater не запущен: {e}")
+
+        try:
+            from .natr_updater import start_natr_updater
+            start_natr_updater()
+        except Exception as e:
+            print(f"⚠️ NATR updater не запущен: {e}")
