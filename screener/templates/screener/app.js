870340ec697e9277cdd17b17233daece15d1640b
RomahaPoleha <romashkoo2023@list.ru>
2026-07-22 16:22:04 +0300

    fix: цвет аллертов

--- screener/templates/screener/app.js
+++ screener/templates/screener/app.js
@@ -594,9 +594,7 @@ function handleChartClick(param) {
             lineWidth: 2,
             lineStyle: LightweightCharts.LineStyle.Dashed,
             axisLabelVisible: true,
-            axisLabelColor: '#000000',
-            axisLabelBackgroundColor: '#f0b90b',
-            title: ` ${price.toFixed(currentPrecision)}`
+            title: `🔔 ${price.toFixed(currentPrecision)}`
         });
         activeAlerts.push({ price: price, line: line, active: true });
     }
